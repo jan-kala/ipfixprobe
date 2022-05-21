a445d9ef4e8306f4cc16f5b97c3bd0c3597deb5e
Jan Kala <jan.kala@jamf.com>
2022-05-21 17:32:03 +0200

    added webtraffic annotator module

---
+++ process/webtraffic.cpp
@@ -0,0 +1,231 @@
+/**
+ * \file webtraffic.cpp
+ * \brief Plugin for parsing webtraffic traffic.
+ * \author Jan Kala <xkalaj01@stud.fit.vutbr.cz>
+ * \date 2022
+ */
+/*
+ * Copyright (C) 2022 CESNET
+ *
+ * LICENSE TERMS
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of the Company nor the names of its contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * ALTERNATIVELY, provided that this notice is retained in full, this
+ * product may be distributed under the terms of the GNU General Public
+ * License (GPL) version 2 or later, in which case the provisions
+ * of the GPL apply INSTEAD OF those given above.
+ *
+ * This software is provided as is'', and any express or implied
+ * warranties, including, but not limited to, the implied warranties of
+ * merchantability and fitness for a particular purpose are disclaimed.
+ * In no event shall the company or contributors be liable for any
+ * direct, indirect, incidental, special, exemplary, or consequential
+ * damages (including, but not limited to, procurement of substitute
+ * goods or services; loss of use, data, or profits; or business
+ * interruption) however caused and on any theory of liability, whether
+ * in contract, strict liability, or tort (including negligence or
+ * otherwise) arising in any way out of the use of this software, even
+ * if advised of the possibility of such damage.
+ *
+ */
+
+#include <iostream>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "webtraffic.hpp"
+
+namespace ipxp {
+
+int RecordExtWEBTRAFFIC::REGISTERED_ID = -1;
+
+__attribute__((constructor)) static void register_this_plugin()
+{
+   static PluginRecord rec = PluginRecord("webtraffic", [](){return new WEBTRAFFICPlugin();});
+   register_plugin(&rec);
+   RecordExtWEBTRAFFIC::REGISTERED_ID = register_extension();
+}
+
+WEBTRAFFICPlugin::WEBTRAFFICPlugin() : manager(nullptr), numberOfSuccessfullRequests(0)
+{
+   std::cout << "ctor" << std::endl;
+}
+
+WEBTRAFFICPlugin::WEBTRAFFICPlugin(const WEBTRAFFICPlugin &p)
+{
+   std::cout << "ctor 2" << std::endl;
+
+   init(nullptr);
+}
+
+WEBTRAFFICPlugin::~WEBTRAFFICPlugin()
+{
+   std::cout << "destruct" << std::endl;
+
+   close();
+}
+
+void WEBTRAFFICPlugin::init(const char *params)
+{
+   std::cout << "init" << std::endl;
+   manager = new WebtrafficRequestManager();
+}
+
+void WEBTRAFFICPlugin::close()
+{
+   std::cout << "close" << std::endl;
+
+   if (manager != nullptr){
+      delete manager;
+      manager = nullptr;
+   }
+}
+
+ProcessPlugin *WEBTRAFFICPlugin::copy()
+{
+   std::cout << "copy" << std::endl;
+
+   return new WEBTRAFFICPlugin(*this);
+}
+
+void WEBTRAFFICPlugin::pre_export(Flow &rec)
+{
+   std::cout << "pre_export" << std::endl;
+
+   // tady prekroutim flow data na to co potrebuju:
+   WebtrafficRequestData data(rec);
+   
+   // request managerovi predhodim zadost s daty vyssie
+      // pokud uspeje, tak vytvorim novy record jak v osquer
+      // rec.add_extension(record);
+   manager->readInfoAboutWebTraffic(data);
+
+}
+
+WebtrafficRequestData::WebtrafficRequestData(Flow &rec)
+{
+   if (rec.ip_version == 4){
+      char ipString[INET_ADDRSTRLEN];
+
+      inet_ntop(AF_INET, &(rec.src_ip.v4), ipString, INET_ADDRSTRLEN);
+      src_ip = ipString;
+
+      inet_ntop(AF_INET, &(rec.dst_ip.v4), ipString, INET_ADDRSTRLEN);
+      dst_ip = ipString;
+   } else {
+      char ipString[INET6_ADDRSTRLEN];
+
+      inet_ntop(AF_INET6, &(rec.src_ip.v6), ipString, INET6_ADDRSTRLEN);
+      src_ip = ipString;
+
+      inet_ntop(AF_INET6, &(rec.dst_ip.v6), ipString, INET6_ADDRSTRLEN);
+      dst_ip = ipString;
+   }
+
+   src_port = rec.src_port;
+   dst_port = rec.dst_port;
+   uint64_t ts_start_micro = (rec.time_first.tv_sec * (uint64_t)1000000) + (rec.time_first.tv_usec); 
+   uint64_t ts_end_micro = (rec.time_last.tv_sec * (uint64_t)1000000) + (rec.time_last.tv_usec); 
+
+   ts_middle = (ts_start_micro + ts_end_micro) / 2;
+}
+
+WebtrafficRequestManager::WebtrafficRequestManager()
+{
+   std::cout << "Request Manager ctor" << std::endl;
+}
+
+WebtrafficRequestManager::~WebtrafficRequestManager()
+{
+   std::cout << "Request Manager dtor" << std::endl;
+   close(sockFd);
+   sockFd = 0;
+}
+
+void WebtrafficRequestManager::connectToDispatcher(int port)
+{
+   int sock = 0;
+   struct sockaddr_in remote;
+
+   sock = socket(AF_INET, SOCK_STREAM, 0);
+   if (sock == -1){
+      throw std::runtime_error("Webtraffic: Failed to create socket.");
+   }
+
+   remote.sin_family = AF_INET;
+   inet_pton(AF_INET, "127.0.0.1", &(remote.sin_addr.s_addr));
+   remote.sin_port = htons(port);
+   
+   if (connect(sock, (struct sockaddr*)&remote, sizeof(remote)) == -1){
+      throw std::runtime_error("Webtraffic: Failed to connect to the client!");
+   }
+
+   sockFd = sock;
+}
+
+void WebtrafficRequestManager::readInfoAboutWebTraffic(WebtrafficRequestData &data)
+{
+   // connect 
+   connectToDispatcher(50555);
+
+   // construct request
+   json newRequest;
+   newRequest["srcIp"] = data.src_ip;
+   newRequest["srcPort"] = data.src_port;
+   newRequest["dstIp"] = data.dst_ip;
+   newRequest["dstPort"] = data.dst_port;
+   newRequest["timestamp"] = data.ts_middle;
+
+   // transfer json request to data
+   auto response_string = newRequest.dump();
+   int32_t payload_data_size = response_string.length();
+   size_t total_payload_size = 4 + payload_data_size;
+
+   char payload[total_payload_size];
+   memset(payload, '\0', total_payload_size);
+
+   memcpy(payload, (u_char *)&payload_data_size, 4);
+   memcpy(payload+4, response_string.data(), response_string.length());
+  
+   // send it!
+   if (send(sockFd, payload, total_payload_size, 0) < 0){
+      throw std::runtime_error("Webtraffic: Failed to send request to dispatcher");
+   }
+
+   // read response
+   size_t response_len = 0;
+   u_char len_buffer[4];
+
+   response_len = recv(sockFd, len_buffer, 4, 0);
+   if (response_len == -1){
+      throw std::runtime_error("Webtrafic: Failed to receive response length.");
+   }
+
+   auto response_json_len = (uint32_t)*len_buffer;
+   char response_json[response_json_len];
+   memset(response_json, '\0', response_json_len);
+
+   recv(sockFd, response_json, response_json_len, 0);
+
+   json response = json::parse(std::string(response_json, response_json_len));
+
+   std::cout << response.dump() << std::endl;
+
+
+}
+
+}
+
---
+++ process/webtraffic.hpp
@@ -0,0 +1,192 @@
+/**
+ * \file webtraffic.hpp
+ * \brief Plugin for parsing webtraffic traffic.
+ * \author Jan Kala <xkalaj01@stud.fit.vutbr.cz>
+ * \date 2022
+ */
+/*
+ * Copyright (C) 2022 CESNET
+ *
+ * LICENSE TERMS
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of the Company nor the names of its contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * ALTERNATIVELY, provided that this notice is retained in full, this
+ * product may be distributed under the terms of the GNU General Public
+ * License (GPL) version 2 or later, in which case the provisions
+ * of the GPL apply INSTEAD OF those given above.
+ *
+ * This software is provided as is'', and any express or implied
+ * warranties, including, but not limited to, the implied warranties of
+ * merchantability and fitness for a particular purpose are disclaimed.
+ * In no event shall the company or contributors be liable for any
+ * direct, indirect, incidental, special, exemplary, or consequential
+ * damages (including, but not limited to, procurement of substitute
+ * goods or services; loss of use, data, or profits; or business
+ * interruption) however caused and on any theory of liability, whether
+ * in contract, strict liability, or tort (including negligence or
+ * otherwise) arising in any way out of the use of this software, even
+ * if advised of the possibility of such damage.
+ *
+ */
+
+#ifndef IPXP_PROCESS_WEBTRAFFIC_HPP
+#define IPXP_PROCESS_WEBTRAFFIC_HPP
+
+#include <string>
+#include <sstream>
+#include <cstring>
+#include <nlohmann/json.hpp>
+
+#ifdef WITH_NEMEA
+  #include "fields.h"
+#endif
+
+#include <ipfixprobe/process.hpp>
+#include <ipfixprobe/flowifc.hpp>
+#include <ipfixprobe/packet.hpp>
+#include <ipfixprobe/ipfix-elements.hpp>
+
+#define DEFAULT_FILL_TEXT "UNDEFINED"
+
+#define WEBTRAFFIC_UNIREC_TEMPLATE \
+   "WEBTRAFFIC_HOSTNAME" /* TODO: unirec template */
+
+UR_FIELDS (
+   /* TODO: unirec fields definition */
+   string WEBTRAFFIC_HOSTNAME,
+)
+
+namespace ipxp {
+
+using namespace nlohmann;
+
+/**
+ * \brief Flow record extension header for storing parsed WEBTRAFFIC data.
+ */
+struct RecordExtWEBTRAFFIC : public RecordExt {
+   static int REGISTERED_ID;
+   std::string    hostname;
+
+   RecordExtWEBTRAFFIC() : RecordExt(REGISTERED_ID)
+   {
+      hostname = DEFAULT_FILL_TEXT;
+   }
+
+   RecordExtWEBTRAFFIC(const RecordExtWEBTRAFFIC *record) : RecordExt(REGISTERED_ID)
+   {
+      hostname = record->hostname;
+   }
+   
+#ifdef WITH_NEMEA
+   virtual void fill_unirec(ur_template_t *tmplt, void *record)
+   {
+   }
+
+   const char *get_unirec_tmplt() const
+   {
+      return WEBTRAFFIC_UNIREC_TEMPLATE;
+   }
+#endif // ifdef WITH_NEMEA
+
+   virtual int fill_ipfix(uint8_t *buffer, int size)
+   {
+      int length, total_length = 0;
+
+      // WEBTRAFFIC_HOSTNAME
+      length = hostname.length();
+      if (total_length + length + 1 > size){
+         return -1;
+      }
+      buffer[total_length] = length;
+      memcpy(buffer + total_length + 1, hostname.c_str(), length);
+      total_length += length + 1;
+
+      return total_length;
+   }
+
+   const char **get_ipfix_tmplt() const
+   {
+      static const char *ipfix_template[] = {
+         IPFIX_WEBTRAFFIC_TEMPLATE(IPFIX_FIELD_NAMES)
+         nullptr
+      };
+      return ipfix_template;
+   }
+
+   std::string get_text() const
+   {
+      std::ostringstream out;
+      out << "hostname=\"" << hostname << "\"";
+      return out.str();
+   }
+};
+
+struct WebtrafficRequestData {
+   WebtrafficRequestData(Flow &rec);
+
+   std::string src_ip;
+   uint32_t    src_port;
+   std::string dst_ip;
+   uint32_t    dst_port;
+   uint64_t    ts_middle;
+};
+
+struct WebtrafficRequestManager {
+   WebtrafficRequestManager();
+
+   ~WebtrafficRequestManager();
+
+   const RecordExtWEBTRAFFIC *getRecord(){ return recWebtraffic; }
+
+   void readInfoAboutWebTraffic(WebtrafficRequestData &data);
+
+private:
+   void connectToDispatcher(int port);
+
+
+   int                     sockFd;
+   RecordExtWEBTRAFFIC *   recWebtraffic;
+};
+
+/**
+ * \brief Process plugin for parsing WEBTRAFFIC packets.
+ */
+class WEBTRAFFICPlugin : public ProcessPlugin
+{
+public:
+   WEBTRAFFICPlugin();
+   ~WEBTRAFFICPlugin();
+   WEBTRAFFICPlugin(const WEBTRAFFICPlugin &p);
+   void init(const char *params);
+   void close();
+   OptionsParser *get_parser() const { return new OptionsParser("webtraffic", "Parse WEBTRAFFIC traffic"); }
+   std::string get_name() const { return "webtraffic"; }
+   RecordExt *get_ext() const { return new RecordExtWEBTRAFFIC(); }
+   ProcessPlugin *copy();
+
+   // int pre_create(Packet &pkt);
+   // int post_create(Flow &rec, const Packet &pkt);
+   // int pre_update(Flow &rec, Packet &pkt);
+   // int post_update(Flow &rec, const Packet &pkt);
+   void pre_export(Flow &rec);
+
+private:
+   WebtrafficRequestManager *manager;
+   int numberOfSuccessfullRequests;
+};
+
+}
+#endif /* IPXP_PROCESS_WEBTRAFFIC_HPP */
+
